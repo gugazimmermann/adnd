37301f3ee9a44a4ff0b6bb3d67a8c343f4a12725
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-13 15:28:24 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Loading } from "./components";
+
+const NotFound = lazy(() => import("./pages/not-found/NotFound"));
+const AuthLayout = lazy(() => import("./pages/auth/AuthLayout"));
+const SignIn = lazy(() => import("./pages/auth/SignIn"));
+const ForgotPassword = lazy(() => import("./pages/auth/ForgotPassword"));
+const RedefinePassword = lazy(() => import("./pages/auth/RedefinePassword"));
+const SignUp = lazy(() => import("./pages/auth/SignUp"));
+const ConfirmSignUp = lazy(() => import("./pages/auth/ConfirmSignUp"));
+
+const Layout = lazy(() => import("./pages/layout/Layout"));
+const Home = lazy(() => import("./pages/home/Home"));
+const Profile = lazy(() => import("./pages/profile/Profile"));
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/forgorpassword" element={<ForgotPassword />} />
+          <Route path="/redefinepassword" element={<RedefinePassword />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/confirmsignup" element={<ConfirmSignUp />} />
+        </Route>
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
