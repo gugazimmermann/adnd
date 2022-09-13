ec223f98151093ce4f65232bca4bd52b6f16675f
Guga Zimmermann <gugazimmermann@gmail.com>
2022-09-13 15:26:39 -0300

    amplify, browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
-import ReactDOM from 'react-dom/client';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import App from "./App";
+import "./index.css";
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 );
 
 serviceWorkerRegistration.register();
